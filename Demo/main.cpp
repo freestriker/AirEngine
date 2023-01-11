dac47c719b5103086b57fa619116f5e010853958
FREEstriker <2317416569@qq.com>
2023-01-11 22:20:19 +0800

    设置xmake参数

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,17 +1,20 @@
 ﻿#pragma once
 #include <QApplication>
-#include <qwindow.h>
+#include <QWindow>
 #include "../Runtime/Core/Bootstrapper.hpp"
 #include <iostream>
 #include "../Runtime/Core/Manager/GraphicDeviceManager.hpp"
-using namespace std;
+#include <qdebug.h>
+
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
     QWindow* window = new QWindow();
-    window->show();
+    window->show();    
+
     std::unique_ptr<AirEngine::Runtime::Core::Bootstrapper> bootstrapper = std::unique_ptr<AirEngine::Runtime::Core::Bootstrapper>(new AirEngine::Runtime::Core::Bootstrapper());
     bootstrapper->Boot();
+
     return app.exec();
 }
--- Runtime/xmake.lua
+++ Runtime/xmake.lua
@@ -1,10 +1,6 @@
-add_requires("vulkansdk")
-add_requires("vk-bootstrap")
 
 target("Runtime")
     add_rules("qt.shared")
     add_files("**.cpp")
     add_headerfiles(("**.hpp"))
-    add_packages("vulkansdk")
-    add_packages("vk-bootstrap")
     add_defines("DLL_EXPORTS")
