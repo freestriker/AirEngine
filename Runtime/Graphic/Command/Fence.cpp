57f513e6ef4b581b0e14382e1da06ccaa33ff0ee
FREEstriker <37498703+FREEstriker@users.noreply.github.com>
2023-04-09 16:54:53 +0800

    添加信号量

---
+++ Runtime/Graphic/Command/Fence.cpp
@@ -0,0 +1,38 @@
+﻿#include "Fence.hpp"
+#include "../../Core/Manager/GraphicDeviceManager.hpp"
+
+AirEngine::Runtime::Graphic::Command::Fence::Fence()
+	: Fence(false)
+{
+}
+
+AirEngine::Runtime::Graphic::Command::Fence::Fence(bool isSignaled)
+{
+    VkFenceCreateInfo fenceInfo{};
+    fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+    fenceInfo.flags = isSignaled ? VkFenceCreateFlagBits::VK_FENCE_CREATE_SIGNALED_BIT : 0;
+    auto result = vkCreateFence(Core::Manager::GraphicDeviceManager::VkDevice(), &fenceInfo, nullptr, &_vkFence);
+    if (result != VK_SUCCESS) qFatal("Failed to create fence.");
+}
+
+AirEngine::Runtime::Graphic::Command::Fence::~Fence()
+{
+    vkDestroyFence(Core::Manager::GraphicDeviceManager::VkDevice(), _vkFence, nullptr);
+}
+
+void AirEngine::Runtime::Graphic::Command::Fence::Reset()
+{
+    auto result = vkResetFences(Core::Manager::GraphicDeviceManager::VkDevice(), 1, &_vkFence);
+    if (result != VK_SUCCESS) qFatal("Failed to reset fence.");
+}
+
+void AirEngine::Runtime::Graphic::Command::Fence::Wait()
+{
+    auto result = vkWaitForFences(Core::Manager::GraphicDeviceManager::VkDevice(), 1, &_vkFence, VK_TRUE, UINT64_MAX);
+    if (result != VK_SUCCESS) qFatal("Failed to wait fence.");
+}
+
+VkResult AirEngine::Runtime::Graphic::Command::Fence::Status()
+{
+    return vkGetFenceStatus(Core::Manager::GraphicDeviceManager::VkDevice(), _vkFence);
+}
---
+++ Runtime/Graphic/Command/Fence.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <vulkan/vulkan_core.h>
+#include "../../Utility/ContructorMacro.hpp"
+#include "../../Utility/ExportMacro.hpp"
+
+namespace AirEngine
+{
+	namespace Runtime
+	{
+		namespace Graphic
+		{
+			namespace Command
+			{
+				class DLL_API Fence final
+				{
+				private:
+					VkFence _vkFence;
+				public:
+					Fence();
+					Fence(bool isSignaled);
+					~Fence();
+					NO_COPY_MOVE(Fence)
+
+					inline VkFence& VkHandle()
+					{
+						return _vkFence;
+					}
+					void Reset();
+					void Wait();
+					VkResult Status();
+				};
+			}
+		}
+	}
+}
