280a0cd8d61f39fb593e4305be96b9b9967af721
freestriker <2317416569@qq.com>
2023-05-31 22:48:51 +0800

    添加部分提交支持

--- Runtime/Graphic/Instance/Queue.hpp
+++ Runtime/Graphic/Instance/Queue.hpp
@@ -18,25 +18,45 @@ namespace AirEngine
 		}
 		namespace Graphic
 		{
+			namespace Command
+			{
+				class Semaphore;
+				class CommandBuffer;
+			}
 			namespace Instance
 			{
 				class AIR_ENGINE_API Queue final
 				{
 					friend class Core::Manager::GraphicDeviceManager;
+				public:
+					struct CommandBufferSubmitSemaphoreInfo
+					{
+						Command::Semaphore* semphore;
+						VkPipelineStageFlags2 stageMask;
+					};
+					struct CommandBufferSubmitInfo
+					{
+						std::vector<CommandBufferSubmitSemaphoreInfo> waitInfos;
+						std::vector<Command::CommandBuffer*> commandBuffers;
+						std::vector<CommandBufferSubmitSemaphoreInfo> signalInfos;
+					};
 				private:
-					VkQueue _queue;
+					VkQueue _vkQueue;
 					Utility::Fiber::mutex _mutex;
 					Utility::InternedString _name;
 					uint32_t _familyIndex;
+					std::vector<CommandBufferSubmitInfo> _submitInfos;
 					Queue(VkQueue queue, uint32_t familyIndex, Utility::InternedString name)
-						: _queue(queue)
+						: _vkQueue(queue)
 						, _mutex()
 						, _name(name)
 						, _familyIndex(familyIndex)
+						, _submitInfos()
 					{
 
 					}
 					NO_COPY_MOVE(Queue)
+					void Submit(const std::vector<CommandBufferSubmitInfo>& submitInfos);
 				public:
 					~Queue() = default;
 					inline Utility::Fiber::mutex& Mutex()
@@ -45,7 +65,7 @@ namespace AirEngine
 					}
 					inline VkQueue VkHandle() const
 					{
-						return _queue;
+						return _vkQueue;
 					}
 					inline Utility::InternedString Name()const
 					{
@@ -54,6 +74,34 @@ namespace AirEngine
 					inline uint32_t FamilyIndex()const
 					{
 						return _familyIndex;
+					}
+
+					void ImmediateIndividualSubmit(const CommandBufferSubmitInfo& submitInfo)
+					{
+						std::unique_lock< Utility::Fiber::mutex> lock(_mutex);
+
+						Submit({ submitInfo });
+					}
+					void ImmediateConcentrateSubmit()
+					{
+						std::unique_lock< Utility::Fiber::mutex> lock(_mutex);
+
+						Submit(_submitInfos);
+						_submitInfos.clear();
+					}
+					void ImmediateConcentrateSubmit(const CommandBufferSubmitInfo& submitInfo)
+					{
+						std::unique_lock< Utility::Fiber::mutex> lock(_mutex);
+
+						_submitInfos.emplace_back(submitInfo);
+						Submit(_submitInfos);
+						_submitInfos.clear();
+					}
+					inline void DelayedConcentrateSubmit(const CommandBufferSubmitInfo& submitInfo)
+					{
+						std::unique_lock< Utility::Fiber::mutex> lock(_mutex);
+
+						_submitInfos.emplace_back(submitInfo);
 					}
 				};
 			}
