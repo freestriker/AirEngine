e64bd2f0f9dcbaafe75f5005318dd016b145347a
freestriker <2317416569@qq.com>
2023-06-14 15:20:33 +0800

    变更实现位置

--- Runtime/Asset/Texture2D.cpp
+++ Runtime/Asset/Texture2D.cpp
@@ -5,3 +5,16 @@ RTTR_REGISTRATION
 {
 	rttr::registration::class_<AirEngine::Runtime::Asset::Texture2D>("AirEngine::Runtime::Asset::Texture2D");
 }
+
+AirEngine::Runtime::Asset::Texture2D::Texture2D()
+	: AssetBase()
+	, _image(nullptr)
+{
+
+}
+
+AirEngine::Runtime::Asset::Texture2D::~Texture2D()
+{
+	delete _image;
+	_image = nullptr;
+}
