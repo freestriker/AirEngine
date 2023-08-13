72f4774b37bff15f43b75a356d0a39f4eb8e260c
freestriker <2317416569@qq.com>
2023-08-13 10:13:38 +0800

    添加反射生成

--- Runtime/Graphic/MeshAttributePaser/MeshAttributePaserBase.cpp
+++ Runtime/Graphic/MeshAttributePaser/MeshAttributePaserBase.cpp
@@ -1,1 +1,8 @@
 ﻿#include "MeshAttributePaserBase.hpp"
+REGISTRATION
+{
+	DECLARE_TYPE(AirEngine::Runtime::Graphic::MeshAttributePaser::MeshAttributePaserBase*)
+}
+
+#include "moc_MeshAttributePaserBase.cpp"
+#include "MeshAttributePaserBase.moc"
--- Runtime/Graphic/MeshAttributePaser/PositionNormalTangentTexcoord_MeshAttributePaser.hpp
+++ Runtime/Graphic/MeshAttributePaser/PositionNormalTangentTexcoord_MeshAttributePaser.hpp
@@ -10,15 +10,16 @@ namespace AirEngine
 			namespace MeshAttributePaser
 			{
 				class AIR_ENGINE_API PositionNormalTangentTexcoord_MeshAttributePaser final 
-					: MeshAttributePaserBase
+					: public MeshAttributePaserBase
 				{
+					REFLECTABLE_OBJECT
 				protected:
 					virtual void OnEditPostProcessSteps(uint32_t& postProcessSteps) const override;
 					virtual uint32_t OnGetPerVertexByteSize() const override;
 					virtual void OnPopulateVertexData(void* vertexDataPtr, size_t vertexDataByteSize, const Asset::Mesh::MeshInfo& meshInfo, const aiScene& meshScene) const override;
-					PositionNormalTangentTexcoord_MeshAttributePaser() = default;
-					~PositionNormalTangentTexcoord_MeshAttributePaser() = default;
 				public:
+					INVOKABLE PositionNormalTangentTexcoord_MeshAttributePaser() = default;
+					INVOKABLE~PositionNormalTangentTexcoord_MeshAttributePaser() = default;
 					NO_COPY_MOVE(PositionNormalTangentTexcoord_MeshAttributePaser)
 				};
 			}
