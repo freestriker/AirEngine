78a919282932db7b2a77e2d695ee8bd8c56853ac
freestriker <2317416569@qq.com>
2023-08-15 20:26:37 +0800

    添加Material类

---
+++ Runtime/Graphic/Rendering/Material.cpp
@@ -0,0 +1,12 @@
+﻿#include "Material.hpp"
+
+AirEngine::Runtime::Graphic::Rendering::Material::Material(const Shader& shader)
+	: _shader(&shader)
+	, _bindableAssetInfoMap()
+	, _descriptorSetMemoryInfosMap()
+{
+}
+
+AirEngine::Runtime::Graphic::Rendering::Material::~Material()
+{
+}
---
+++ Runtime/Graphic/Rendering/Material.hpp
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "../../Utility/InternedString.hpp"
+#include "../Manager/DescriptorManagerData.hpp"
+#include "../../Utility/ExportMacro.hpp"
+#include "../../Utility/ContructorMacro.hpp"
+#include <unordered_map>
+#include "MaterialData.hpp"
+
+namespace AirEngine
+{
+	namespace Runtime
+	{
+		namespace Graphic
+		{
+			namespace Rendering
+			{
+				class Shader;
+				class AIR_ENGINE_API Material final
+				{
+				private:
+					const Shader* _shader;
+					std::unordered_multimap<Utility::InternedString, MaterialBindableAssetInfo> _bindableAssetInfoMap;
+					std::unordered_multimap<Utility::InternedString, std::vector<MaterialDescriptorSetMemoryInfo>> _descriptorSetMemoryInfosMap;
+				public:
+					Material(const Shader& shader);
+					~Material();
+					NO_COPY_MOVE(Material);
+
+				};
+			}
+		}
+	}
+}
