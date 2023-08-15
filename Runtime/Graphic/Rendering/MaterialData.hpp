dda0fcb83ea0cd394269b2a562a85642be47e183
freestriker <2317416569@qq.com>
2023-08-15 20:10:08 +0800

    添加material数据结构

---
+++ Runtime/Graphic/Rendering/MaterialData.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "../../Utility/InternedString.hpp"
+#include "../Manager/DescriptorManagerData.hpp"
+
+namespace AirEngine
+{
+	namespace Runtime
+	{
+		namespace Graphic
+		{
+			namespace Rendering
+			{
+				struct MaterialBindableAssetInfo
+				{
+					Utility::InternedString name;
+					void* bindable;
+					uint16_t index;
+				};
+				struct MaterialDescriptorSetMemoryInfo
+				{
+					Manager::DescriptorMemoryHandle handle;
+					uint16_t dynamicCount;
+					uint16_t dynamicCapcity;
+				};
+			}
+		}
+	}
+}
