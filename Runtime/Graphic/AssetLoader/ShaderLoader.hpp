86cebf30d6776f14a80df1f8f8ef8fcef70e44d6
freestriker <2317416569@qq.com>
2024-01-08 13:42:53 +0800

    整理shaderLoader

---
+++ Runtime/Graphic/AssetLoader/ShaderLoader.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "AirEngine/Runtime/AssetLoader/AssetLoaderBase.hpp"
+#include <nlohmann/json.hpp>
+
+namespace AirEngine
+{
+	namespace Runtime
+	{
+		namespace Graphic
+		{
+			namespace Rendering
+			{
+				class Shader;
+			}
+			namespace AssetLoader
+			{
+				class AIR_ENGINE_API ShaderLoader final
+					: public AirEngine::Runtime::AssetLoader::AssetLoaderBase
+				{
+				private:
+					Runtime::Asset::AssetBase* OnLoadAsset(const std::string& path, std::shared_future<void>& loadOperationFuture, bool& isInLoading) override;
+					void OnUnloadAsset(Runtime::Asset::AssetBase* asset) override;
+					static void PopulateShader(AirEngine::Runtime::Graphic::Rendering::Shader* shader, const std::string path, bool* isInLoading);
+				public:
+					ShaderLoader();
+					virtual ~ShaderLoader();
+					NO_COPY_MOVE(ShaderLoader);
+				};
+			}
+		}
+	}
+}
