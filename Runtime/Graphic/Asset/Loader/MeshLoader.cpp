dfdae50acb856fc1c9d85ad850826ddc99ceb1c9
freestriker <2317416569@qq.com>
2024-01-17 16:20:49 +0800

    替换assimp解析器

--- Runtime/Graphic/Asset/Loader/MeshLoader.cpp
+++ Runtime/Graphic/Asset/Loader/MeshLoader.cpp
@@ -14,55 +14,9 @@
 #include <glm/vec3.hpp>
 #include "AirEngine/Runtime/Graphic/MeshAttributePaser/MeshAttributePaserBase.hpp"
 #include "AirEngine/Runtime/Utility/ReflectableObject.hpp"
+#include "AirEngine/Runtime/Utility/StringToAssimpTypeTransfer.hpp"
 #include "AirEngine/Runtime/Graphic/Command/Barrier.hpp"
 #include "AirEngine/Runtime/Core/Manager/TaskManager.hpp"
-
-const static std::unordered_map<std::string, uint32_t> AI_POST_PROCESS_STEPS_MAP
-{
-	{ "aiProcess_CalcTangentSpace", aiProcess_CalcTangentSpace },
-	{ "aiProcess_JoinIdenticalVertices", aiProcess_JoinIdenticalVertices },
-	{ "aiProcess_MakeLeftHanded", aiProcess_MakeLeftHanded },
-	{ "aiProcess_Triangulate", aiProcess_Triangulate },
-	{ "aiProcess_RemoveComponent", aiProcess_RemoveComponent },
-	{ "aiProcess_GenNormals", aiProcess_GenNormals },
-	{ "aiProcess_GenSmoothNormals", aiProcess_GenSmoothNormals },
-	{ "aiProcess_SplitLargeMeshes", aiProcess_SplitLargeMeshes },
-	{ "aiProcess_PreTransformVertices", aiProcess_PreTransformVertices },
-	{ "aiProcess_LimitBoneWeights", aiProcess_LimitBoneWeights },
-	{ "aiProcess_ValidateDataStructure", aiProcess_ValidateDataStructure },
-	{ "aiProcess_ImproveCacheLocality", aiProcess_ImproveCacheLocality },
-	{ "aiProcess_RemoveRedundantMaterials", aiProcess_RemoveRedundantMaterials },
-	{ "aiProcess_FixInfacingNormals", aiProcess_FixInfacingNormals },
-	{ "aiProcess_PopulateArmatureData", aiProcess_PopulateArmatureData },
-	{ "aiProcess_SortByPType", aiProcess_SortByPType },
-	{ "aiProcess_FindDegenerates", aiProcess_FindDegenerates },
-	{ "aiProcess_FindInvalidData", aiProcess_FindInvalidData },
-	{ "aiProcess_GenUVCoords", aiProcess_GenUVCoords },
-	{ "aiProcess_TransformUVCoords", aiProcess_TransformUVCoords },
-	{ "aiProcess_FindInstances", aiProcess_FindInstances },
-	{ "aiProcess_OptimizeMeshes", aiProcess_OptimizeMeshes },
-	{ "aiProcess_OptimizeGraph", aiProcess_OptimizeGraph },
-	{ "aiProcess_FlipUVs", aiProcess_FlipUVs },
-	{ "aiProcess_FlipWindingOrder", aiProcess_FlipWindingOrder },
-	{ "aiProcess_SplitByBoneCount", aiProcess_SplitByBoneCount },
-	{ "aiProcess_Debone", aiProcess_Debone },
-	{ "aiProcess_GlobalScale", aiProcess_GlobalScale },
-	{ "aiProcess_EmbedTextures", aiProcess_EmbedTextures },
-	{ "aiProcess_ForceGenNormals", aiProcess_ForceGenNormals },
-	{ "aiProcess_DropNormals", aiProcess_DropNormals },
-	{ "aiProcess_GenBoundingBoxes", aiProcess_GenBoundingBoxes },
-	{ "aiProcess_ConvertToLeftHanded", aiProcess_ConvertToLeftHanded },
-	{ "aiProcessPreset_TargetRealtime_Fast", aiProcessPreset_TargetRealtime_Fast },
-	{ "aiProcessPreset_TargetRealtime_Quality", aiProcessPreset_TargetRealtime_Quality },
-	{ "aiProcessPreset_TargetRealtime_MaxQuality", aiProcessPreset_TargetRealtime_MaxQuality }
-};
-
-constexpr uint32_t DEFAULT_POST_PROCESS_STEPS =
-	aiProcess_Triangulate |
-	aiProcess_ImproveCacheLocality |
-	0;
-constexpr uint32_t INVALID_POST_PROCESS_STEPS =
-	~(aiProcess_PreTransformVertices);
 
 AirEngine::Runtime::Asset::AssetBase* AirEngine::Runtime::Graphic::Asset::Loader::MeshLoader::OnLoadAsset(const std::string& path, std::shared_future<void>& loadOperationFuture, bool& isInLoading)
 {
@@ -128,15 +82,10 @@ void AirEngine::Runtime::Graphic::Asset::Loader::MeshLoader::PopulateMesh(AirEng
 		meshAttributePaser.reset(ptr);
 	}
 	//Parse descriptor data
-	uint32_t postProcessSteps = DEFAULT_POST_PROCESS_STEPS;
-	{
-		for (const auto& postProcessStep : descriptor.postProcessSteps)
-		{
-			postProcessSteps |= AI_POST_PROCESS_STEPS_MAP.at(postProcessStep);
-		}
-		meshAttributePaser->OnEditPostProcessSteps(postProcessSteps);
-		postProcessSteps &= INVALID_POST_PROCESS_STEPS;
-	}
+	uint32_t postProcessSteps = Utility::StringToAssimpTypeTransfer::DEFAULT_POST_PROCESS_STEPS;
+	postProcessSteps |= Utility::StringToAssimpTypeTransfer::PostProcessStepsStringsToPostProcessSteps(descriptor.postProcessSteps);
+	meshAttributePaser->OnEditPostProcessSteps(postProcessSteps);
+	postProcessSteps = Utility::StringToAssimpTypeTransfer::RemoveInvalidPostProcessSteps(postProcessSteps);
 
 	Assimp::Importer importer{};
 	{
---
+++ Runtime/Utility/StringToAssimpTypeTransfer.hpp
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include "ContructorMacro.hpp"
+#include "ExportMacro.hpp"
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <assimp/postprocess.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+
+namespace AirEngine
+{
+	namespace Runtime
+	{
+		namespace Utility
+		{
+			class AIR_ENGINE_API StringToAssimpTypeTransfer final
+			{
+			private:
+				StringToAssimpTypeTransfer();
+				~StringToAssimpTypeTransfer() = default;
+				NO_COPY_MOVE(StringToAssimpTypeTransfer);
+
+			public:
+				static constexpr uint32_t DEFAULT_POST_PROCESS_STEPS =
+					aiProcess_Triangulate |
+					aiProcess_ImproveCacheLocality |
+					0;
+				static constexpr uint32_t INVALID_POST_PROCESS_STEPS =
+					~(aiProcess_PreTransformVertices);
+			private:
+				const static std::unordered_map<std::string, uint32_t> AI_POST_PROCESS_STEPS_MAP;
+			public:
+				inline static uint32_t PostProcessStepsStringsToPostProcessSteps(const std::vector<std::string>& postProcessStepsStrings)
+				{
+					uint32_t postProcessSteps = DEFAULT_POST_PROCESS_STEPS;
+					for (const auto& postProcessStep : postProcessStepsStrings)
+					{
+						postProcessSteps |= AI_POST_PROCESS_STEPS_MAP.at(postProcessStep);
+					}
+					postProcessSteps &= INVALID_POST_PROCESS_STEPS;
+
+					return postProcessSteps;
+				}
+				inline static uint32_t RemoveInvalidPostProcessSteps(uint32_t postProcessSteps)
+				{
+					return postProcessSteps &= INVALID_POST_PROCESS_STEPS;
+				}
+			};
+		}
+	}
+}
